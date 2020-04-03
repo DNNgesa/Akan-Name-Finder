91ee92e6a10fd6015147210c93ee4a8ebb56af20
DNNgesa <pro.drakengesa@gmail.com>
2020-04-03 21:33:13 +0300

    replace css file with bootstrap file

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function validate(){
-    var DD = parseInt(document.forms["myForm"]["bd"].value);
-    var MM = parseInt(document.forms["myForm"]["mnth"].value);
-    var YY = parseInt((document.forms["myForm"]["yr"].value).slice(2, 4));
-    var CC = parseInt((document.forms["myForm"]["yr"].value).slice(0, 2));
+    var DD = parseInt(document.forms["dateForm"]["bd"].value);
+    var MM = parseInt(document.forms["dateForm"]["mnth"].value);
+    var YY = parseInt((document.forms["dateForm"]["yr"].value).slice(2, 4));
+    var CC = parseInt((document.forms["dateForm"]["yr"].value).slice(0, 2));
     var MF = document.getElementById('myGender');
     
     var day = (((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD) % 7;
